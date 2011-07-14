76b6d05562e13f914fb11229724f3989f33c287f
James Harton <jamesotron@gmail.com>
2011-07-15 10:45:15 +1200

    Paypal aint threadsafe, which is a bitch in production.

--- lib/paypal/config.rb
+++ lib/paypal/config.rb
@@ -1,7 +1,10 @@
 module Paypal
   module Config
+
+    @@paypal_config = {}
+
     def self.ipn_urls
-      Thread.current[:ipn_urls] ||= {
+      @@paypal_config[:ipn_urls] ||= {
         :sandbox => "https://www.sandbox.paypal.com/cgi-bin/webscr",
         :production => "https://www.paypal.com/cgi-bin/webscr"
       }
@@ -9,10 +12,10 @@ module Paypal
 
     def self.mode=(new_mode)
       raise ArgumentError.new("Paypal::Config.mode should be either :sandbox or :production (you tried to set it as : #{new_mode})") unless [:sandbox, :production].include?(new_mode.to_sym)
-      Thread.current[:mode] = new_mode.to_sym
+      @@paypal_config[:mode] = new_mode.to_sym
     end
     def self.mode
-      Thread.current[:mode] ||= :sandbox
+      @@paypal_config[:mode] ||= :sandbox
     end
     
     def self.ipn_url
@@ -28,17 +31,17 @@ module Paypal
     end
 
     def self.paypal_sandbox_cert=(new_cert)
-      Thread.current[:paypal_sandbox_cert] = new_cert
+      @@paypal_config[:paypal_sandbox_cert] = new_cert
     end
     def self.paypal_sandbox_cert
-      Thread.current[:paypal_sandbox_cert] ||= File.read(File.join(File.dirname(__FILE__), 'certs', 'paypal_sandbox.pem'))
+      @@paypal_config[:paypal_sandbox_cert] ||= File.read(File.join(File.dirname(__FILE__), 'certs', 'paypal_sandbox.pem'))
     end
 
     def self.paypal_production_cert=(new_cert)
-      Thread.current[:paypal_production_cert] = new_cert
+      @@paypal_config[:paypal_production_cert] = new_cert
     end
     def self.paypal_production_cert
-      Thread.current[:paypal_production_cert]
+      @@paypal_config[:paypal_production_cert]
     end
 
     def self.paypal_cert
