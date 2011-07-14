eaa7e3f6cae2b36cbc8637fa45e8c6f3cc7eb056
James Harton <jamesotron@gmail.com>
2011-07-15 11:10:53 +1200

    Try attr_accessor inside class self.

--- lib/paypal/config.rb
+++ lib/paypal/config.rb
@@ -1,48 +1,48 @@
 module Paypal
   module Config
 
-    class << self
+    class >> self
       attr_accessor :ipn_urls, :mode, :paypal_sandbox_cert, :paypal_production_cert, :business_cert, :business_key, :business_cert_id
-    end
 
-    def self.ipn_urls
-      @@ipn_urls ||= {
-        :sandbox => "https://www.sandbox.paypal.com/cgi-bin/webscr",
-        :production => "https://www.paypal.com/cgi-bin/webscr"
-      }
-    end
+      def ipn_urls
+        @@ipn_urls ||= {
+          :sandbox => "https://www.sandbox.paypal.com/cgi-bin/webscr",
+          :production => "https://www.paypal.com/cgi-bin/webscr"
+        }
+      end
 
-    def self.mode=(new_mode)
-      raise ArgumentError.new("Paypal::Config.mode should be either :sandbox or :production (you tried to set it as : #{new_mode})") unless [:sandbox, :production].include?(new_mode.to_sym)
-      @@mode = new_mode.to_sym
-    end
-    def self.mode
-      @@mode ||= :sandbox
-    end
-    
-    def self.ipn_url
-      ipn_urls[mode]
-    end
-    
-    def self.ipn_validation_path
-      URI.parse(ipn_url).path + "?cmd=_notify-validate"
-    end
+      def mode=(new_mode)
+        raise ArgumentError.new("Paypal::Config.mode should be either :sandbox or :production (you tried to set it as : #{new_mode})") unless [:sandbox, :production].include?(new_mode.to_sym)
+        @@mode = new_mode.to_sym
+      end
+      def mode
+        @@mode ||= :sandbox
+      end
 
-    def self.ipn_validation_url
+      def ipn_url
+        ipn_urls[mode]
+      end
+
+      def ipn_validation_path
+        URI.parse(ipn_url).path + "?cmd=_notify-validate"
+      end
+
+      def ipn_validation_url
       "#{ipn_url}?cmd=_notify-validate"
-    end
+      end
 
-    def self.paypal_sandbox_cert
-      @@paypal_sandbox_cert ||= File.read(File.join(File.dirname(__FILE__), 'certs', 'paypal_sandbox.pem'))
-    end
+      def paypal_sandbox_cert
+        @@paypal_sandbox_cert ||= File.read(File.join(File.dirname(__FILE__), 'certs', 'paypal_sandbox.pem'))
+      end
 
-    def self.paypal_cert
-      case mode
-      when :sandbox
-        paypal_sandbox_cert
-      when :production
-        raise StandardError.new("You should set Paypal::Config.paypal_production_cert with your paypal production certificate") unless paypal_production_cert
-        paypal_production_cert
+      def paypal_cert
+        case mode
+        when :sandbox
+          paypal_sandbox_cert
+        when :production
+          raise StandardError.new("You should set Paypal::Config.paypal_production_cert with your paypal production certificate") unless paypal_production_cert
+          paypal_production_cert
+        end
       end
     end
   end
