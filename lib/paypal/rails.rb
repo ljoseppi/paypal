c7943a4f51b31f76e3c1feb58b8069e9736f4032
Lairton Borges <ljoseppi@gmail.com>
2012-03-06 16:26:36 -0300

    Fix for includes of config and notification

--- lib/paypal/rails.rb
+++ lib/paypal/rails.rb
@@ -1,8 +1,8 @@
 require 'active_support'
-require File.join(File.dirname(__FILE__),'helpers', '/config')
+require File.join(File.dirname(__FILE__),'config')
 require File.join(File.dirname(__FILE__),'helpers', '/common')
 require File.join(File.dirname(__FILE__),'helpers', '/rails')
-require File.join(File.dirname(__FILE__),'helpers', '/notification')
+require File.join(File.dirname(__FILE__),'notification')
 
 ActionView::Base.send(:include, Paypal::Helpers::Common)
 ActionView::Base.send(:include, Paypal::Helpers::Rails)
