2dcb09871802356ef117e99ec52c8571bcd389a4
Lairton Borges <ljoseppi@gmail.com>
2012-03-06 16:00:11 -0300

    Include of config and notification.

--- lib/paypal/rails.rb
+++ lib/paypal/rails.rb
@@ -1,6 +1,8 @@
 require 'active_support'
+require File.join(File.dirname(__FILE__),'helpers', '/config')
 require File.join(File.dirname(__FILE__),'helpers', '/common')
 require File.join(File.dirname(__FILE__),'helpers', '/rails')
+require File.join(File.dirname(__FILE__),'helpers', '/notification')
 
 ActionView::Base.send(:include, Paypal::Helpers::Common)
 ActionView::Base.send(:include, Paypal::Helpers::Rails)
